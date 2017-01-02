fcbd92c73944d39e2aecd8f491ba415af9e078d3
Masaki Koyanagi <mascii@gmail.com>
2017-01-02 14:24:28 +0900

    moving files

--- 04-led-button-key-taskmgr/led-button-key-taskmgr/led-button-key-taskmgr.ino
+++ 04-led-button-key-taskmgr/led-button-key-taskmgr/led-button-key-taskmgr.ino
@@ -6,20 +6,21 @@ const int outputLed = 1;
 bool outputState = true;
 
 void setup() {
-  pinMode (inputButton,INPUT);
-  pinMode(outputLed,OUTPUT);
+  pinMode (inputButton, INPUT);
+  pinMode(outputLed, OUTPUT);
 }
 
-void loop(){
+void loop() {
   if (digitalRead(inputButton) == HIGH) {
-    digitalWrite(outputLed,1);
+    digitalWrite(outputLed, 1);
     if (!outputState) {
       outputState = true;
       DigiKeyboard.update();
-      DigiKeyboard.sendKeyStroke(KEY_ESCAPE, MOD_CONTROL_LEFT | MOD_SHIFT_LEFT);
+      //      DigiKeyboard.sendKeyStroke(KEY_ESCAPE, MOD_CONTROL_LEFT | MOD_SHIFT_LEFT);
+      DigiKeyboard.sendKeyStroke(KEY_4, MOD_CONTROL_LEFT | MOD_SHIFT_LEFT | MOD_SPACE);
     }
   } else {
-    digitalWrite(outputLed,0);
+    digitalWrite(outputLed, 0);
     outputState = false;
   }
   DigiKeyboard.delay(100);
